85aa1518f0b31752837b0ac637427c5c86b487d4
David Jarrin <dmjarrin@gmail.com>
2017-09-13 20:29:59 -0400

    feat: basic components for the project have been started
    Need to loop through the different books in each shelf. Need to start using API to see exactly what I will be working with data wise

---
+++ src/components/Book.js
@@ -0,0 +1,36 @@
+import React , { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Book extends Component {
+    static propTypess = {
+        title: PropTypes.string.isRequired,
+        author: PropTypes.string.isRequired,
+        bookImage: PropTypes.string.isRequired
+    };
+
+    render() {
+        return (
+            <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193 }}>
+                        <img alt="" src={this.props.bookImage}/>
+                    </div>
+                    <div className="book-shelf-changer">
+                        <select>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="book-title">{this.props.title}</div>
+                <div className="book-authors">{this.props.author}</div>
+            </div>
+        )
+    }
+}
+
+
+export default Book
